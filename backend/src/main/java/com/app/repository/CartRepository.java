3eb1a336c63ad243f4fcb06a157bb012163a22b3
saloni <salonisalunkhe123@gmail.com>
2024-03-14 17:31:37 +0530

    Added CartRepository

---
+++ backend/src/main/java/com/app/repository/CartRepository.java
@@ -0,0 +1,13 @@
+package com.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.app.modal.*;
+
+public interface CartRepository extends JpaRepository<Cart,Long>{
+
+	@Query("SELECT c From Cart c where c.user.id=:userId")
+	public Cart findByUserId(@Param("userId")Long userId);
+}
